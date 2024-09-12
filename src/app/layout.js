d9f388a90ad50093a2c8bd7c1cba875d0d9cd7ba
punchpatcha <patcharaluk.klin@gmail.com>
2024-09-12 15:25:10 +0700

    updating ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head></head>
+      <head>  <link rel="icon" href="/favicon.ico" /> </head>
       <body>{children}</body>
     </html>
   );
