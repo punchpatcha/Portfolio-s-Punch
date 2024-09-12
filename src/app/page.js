cd0e4b16014e214bc9e5373ae89136fe42581c85
punchpatcha <patcharaluk.klin@gmail.com>
2024-09-12 14:44:40 +0700

    updating ui

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import styles from "./page.module.css";
 
 export default function Home() {
   const images = [
-    "/cover/image1.jpg",
+    "/cover/image1.png",
     "/cover/image2.jpg",
     "/cover/image3.jpg",
   ];
